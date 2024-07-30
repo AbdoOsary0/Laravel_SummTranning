ae8fbb33b410783007d54fed484400133f982dfc
Abdelrahman <andelrahman.mohmad@gmail.com>
2024-07-30 23:59:44 +0300

    show data from small array

--- my-app/app/Http/Controllers/PostController.php
+++ my-app/app/Http/Controllers/PostController.php
@@ -11,20 +11,20 @@ class PostController extends Controller
   //
   public function index()
   {
-    //show all Posts
-    // $posts = [
-    //   ['title' => "title1", 'description' => 'This the First Post', 'author' => "Ahmed", "createAt" => "12/10/2020"],
-    //   ['title' => "title2", 'description' => 'This the First Post', 'author' => "Abdo", "createAt" => "12/10/2020"],
-    //   ['title' => "title2", 'description' => 'This the First Post', 'author' => "Mahmoud", "createAt" => "12/10/2020"],
-    //   ['title' => "title3", 'description' => 'This the First Post', 'author' => "Mohammed", "createAt" => "12/10/2020"],
-    //   ['title' => "title4", 'description' => 'This the First Post', 'author' => "Malk", "createAt" => "12/10/2020"],
-    //   ['title' => "title5", 'description' => 'This the First Post', 'author' => "Nora", "createAt" => "12/10/2020"]
-    // ];
-    $posts = DB::table("posts")->get();
+    // show all Posts
+    $posts = [
+      ['title' => "title1", 'description' => 'This the First Post', 'author' => "Ahmed", "createAt" => "12/10/2020"],
+      ['title' => "title2", 'description' => 'This the First Post', 'author' => "Abdo", "createAt" => "12/10/2020"],
+      ['title' => "title2", 'description' => 'This the First Post', 'author' => "Mahmoud", "createAt" => "12/10/2020"],
+      ['title' => "title3", 'description' => 'This the First Post', 'author' => "Mohammed", "createAt" => "12/10/2020"],
+      ['title' => "title4", 'description' => 'This the First Post', 'author' => "Malk", "createAt" => "12/10/2020"],
+      ['title' => "title5", 'description' => 'This the First Post', 'author' => "Nora", "createAt" => "12/10/2020"]
+    ];
+    // $posts = DB::table("posts")->get();
     return view("posts.show", ['posts' => $posts]);
   }
   function create()
   {
-    
+    return view('posts.create');
   }
 }
--- my-app/resources/views/posts/create.blade.php
+++ my-app/resources/views/posts/create.blade.php
@@ -2,20 +2,22 @@
 
 @section('create')
     <h1>Hello Create Page</h1>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">title</label>
-            <input type="text" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="input-group">
-            <label for="">Content</label>
-            <textarea class="form-control" aria-label="With textarea"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">title</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="input-group">
+                <label for="">Content</label>
+                <textarea class="form-control" aria-label="With textarea"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
